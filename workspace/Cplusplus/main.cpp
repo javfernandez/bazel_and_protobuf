a0380fc68f36ff06cf1021f1165af738579d8cfc
JuanDVal <juan.valenciano@globant.com>
2024-12-03 13:59:06 -0500

    add proto deps and build bazel

--- workspace/Cplusplus/main.cpp
+++ workspace/Cplusplus/main.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
+#include <ctime>
+#include <fstream>
+#include <google/protobuf/util/time_util.h>
+#include <string>
+
+#include "point.pb.h"
+
+using google::protobuf::util::TimeUtil;
 
 int main(void) {
     std::cout<<"Hello, World!"<< std::endl;
+
+    // my_protos::point::Point p;
+    // p.set_x(123);
+    // p.set_y(456);
+    // std::cout << p.DebugString();
+
     return 0;
 }
